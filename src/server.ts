cabe8ec442d09d8406f8cb6118a484e3c7319da1
Artur Almeida <arturoalmeidafreire@gmail.com>
2022-11-03 18:56:36 -0300

    feat: configuração inicial do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,20 @@
-const a: number = 10;
-console.log(a);
+import * as dotenv from 'dotenv';
+import express from "express";
+import bodyParser from "body-parser";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({ message: "Working!" })
+});
+
+app.use("/api", router);
+
+app.listen(port, () => { console.log(`Running on http://localhost:${port}/api`) });
